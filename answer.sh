63ddc1411676b8926527df64e0fc9fa5d8ea1bf3
admin-farhan <muhfarhan912391@gmail.com>
2023-11-29 14:57:18 +0700

    create answer.sh

---
+++ answer.sh
@@ -0,0 +1,17 @@
+# # 1. Write a MongoDB query to display all the documents in the collection restaurants.
+# # solution
+# db.restaurant.find()
+# or 
+# db.restaurent.find().pretty()
+
+# 2. Write a MongoDB query to display the fields restaurant_id, name, borough and cuisine for all the documents in the collection restaurant.
+# solution
+
+# 3. Write a MongoDB query to display the fields restaurant_id, name, borough and cuisine, but exclude the field _id for all the documents in the collection restaurant.
+# solution
+
+# 4. Write a MongoDB query to display the fields restaurant_id, name, borough and zip code, but exclude the field _id for all the documents in the collection restaurant.
+# solution
+
+# 5. Write a MongoDB query to display all the restaurant which is in the borough Bronx.
+# solution
